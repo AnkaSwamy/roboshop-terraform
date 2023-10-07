0332a0b69685315d51a17105116b4ff2d3667dde
Anka swamy <ankaswamy.telagorla@gmail.com>
2023-10-07 18:04:26 +0530

    terraform using ssm parameters

--- main.tf
+++ main.tf
@@ -1,4 +1,6 @@
-module "test" {
+module "instances" {
+  for_each  = var.components
   source = "git::https://github.com/AnkaSwamy/tf-module-app.git"
-  env    = var.env
+  component = each.key
+  env       = var.env
 }
